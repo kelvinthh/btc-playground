d2a2d5e6f561c0f59629dd5982ab3795e8edd37d
Kelvin Tam <hoheitam@gmail.com>
2022-03-10 05:37:10 +0800

    UI element test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ImageBackground, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
+
+const image = { uri: "https://images.pexels.com/photos/844124/pexels-photo-844124.jpeg" };
+
 
 const App = () => {
+  const [text, setText] = useState("");
+  const [number, setNumber] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>This is running via React Native Web!</Text>
-    </View>
-  );
+    <KeyboardAvoidingView style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View style={styles.content}>
+          <Text style={styles.contentText}>BitCoin Playground for {text}</Text>
+          <View>
+            <TextInput
+              style={styles.input}
+              onChangeText={setText}
+              value={text}
+              placeholder="Name"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={setNumber}
+              value={number}
+              placeholder="No."
+              keyboardType="numeric"
+            />
+          </View>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  )
 }
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    height: "100%"
-  }
+    flex: 1,
+    height: "100%",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  content: {
+    //textAlign: "center",
+    backgroundColor: "#000000c0",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  contentText: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+  },
+  input: {
+    width: 350,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    
+  },
 })
 export default App;
